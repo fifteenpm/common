c60fb4fc2eb68476c17c8aabb27bac0f2b3d4dc2
Jeremy Krinsley <krinsley@gmail.com>
2020-10-20 00:11:49 -0400

    making mesh hook 5

--- UI/Player/VideoPlayerContext.js
+++ UI/Player/VideoPlayerContext.js
@@ -2,23 +2,8 @@ import React, { useState, useMemo, useEffect } from 'react';
 import * as THREE from 'three';
 // import { loadVideoMesh } from "../../../Utils/LegacyLoaders";
 
-const useVideoTexture = ({
-  videoElement, loop,
-  muted, volume, sources,
-  playbackRate,
-
-}) => {
+const useVideoTexture = ({ videoElement }) => {
   // initialize video element
-  // videoElement = videoElement || document.createElement('video');
-  // videoElement.codecs = "avc1.4D401E, mp4a.40.2";
-  // videoElement.playsInline = true;
-  // videoElement.post = "https://dummyimage.com/320x240/ffffff/fff";
-  // videoElement.crossOrigin = 'anonymous';
-  // videoElement.loop = loop;
-  // videoElement.muted = muted;
-  // videoElement.volume = volume;
-  // videoElement.playbackRate = playbackRate;
- 
   // create material from video texture
   let texture = new THREE.VideoTexture(videoElement);
   texture.minFilter = THREE.LinearFilter;
@@ -29,20 +14,8 @@ const useVideoTexture = ({
 
 }
 
-const useVideoElement = () => {
-  const element = useMemo(() => document.createElement("video"));
-
-  return {
-    videoElement: element,
-  }
-}
-
-const VideoPlayerContext = React.createContext([{}, () => { }]);
-
-const VideoPlayerProvider = ({ tracks, videoGeometry, curIdx = 0, loop=true, muted=true, volume=0, playbackRate=1.0, ...props }) => {
-
-
-  const [videoElement, sources] = useMemo(() => {
+const useVideoElement = ({ sources, loop = true, muted = true, volume = 0, playbackRate = 1.0, ...props }) => {
+  const videoElement = useMemo(() => {
     const element = document.createElement("video")
     element.codecs = "avc1.4D401E, mp4a.40.2";
     element.playsInline = true;
@@ -51,20 +24,30 @@ const VideoPlayerProvider = ({ tracks, videoGeometry, curIdx = 0, loop=true, mut
     element.loop = loop;
     element.muted = muted;
     element.volume = volume;
-    element.playbackRate = playbackRate; 
-    const currentSources = tracks[curIdx].sources;
-    for (let i = 0; i < currentSources.length; i++) {
+    element.playbackRate = playbackRate;
+    for (let i = 0; i < sources.length; i++) {
       /* First source element creation */
       let src = document.createElement("source");
       // Attribute settings for my first source
-      src.setAttribute("src", currentSources[i].src);
-      src.setAttribute("type", currentSources[i].type);
+      src.setAttribute("src", sources[i].src);
+      src.setAttribute("type", sources[i].type);
       element.appendChild(src);
     }
     document.body.appendChild(element);
-    return [element, currentSources]
+    return element;
   });
-  const { videoTexture } = useVideoTexture({ videoElement, sources });
+
+  return {
+    videoElement
+  }
+}
+
+const VideoPlayerContext = React.createContext([{}, () => { }]);
+
+const VideoPlayerProvider = ({ tracks, videoGeometry, curIdx = 0, ...props }) => {
+
+  const { videoElement } = useVideoElement({ sources: tracks[curIdx].sources })
+  const { videoTexture } = useVideoTexture({ videoElement });
 
   const [state, setState] = useState({
     videoElement: videoElement,
